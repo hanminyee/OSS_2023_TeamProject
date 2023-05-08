99f1ba821ec4ad4b1bbdb7e92c2047fd8ae1ed9c
한민 <hanmin0786@gmail.com>
2023-05-09 00:13:46 +0900

    재재커밋..

--- function.c
+++ function.c
@@ -7,6 +7,33 @@ void Modify(library *list[], int count);
 void Delete(library *list[], int count);
 void Recover(library *list[], int count);
 void Search(library *list[], int count);
-void LoadData();
+int LoadData(library *list[]){
+    int count = 0; //파일에 몇개 데이터 있는지 리턴해야함
+    FILE* fp = fopen("Library_Info.csv", "rt"); //파일 열기
+
+    char dummy[400]; //첫줄 없애기용 더미
+    fgets(dummy, 400, fp); //첫줄 없애기
+    
+    if(fp==NULL){ //파일이 없을 때
+        printf("==>파일이 없습니다!\n");
+        return 0; 
+    }
+    else{ //정상적으로 들어왔을 때
+        while(1){
+            if(feof(fp)){
+                break;
+            }
+            else{
+                char val[400];
+                fgets(val, 400, fp);
+                char *p = strtok(val, ",");
+                while(p!=NULL){
+                    printf("%s", p);
+                    p = strtok(NULL, ",");
+                }
+            }
+        }
+    }
+}
 void SaveData(library *list[], int count);
 void Recommendation(library *list[], int count);
