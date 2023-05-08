de1921ce57408fb5402f492d2836dd6c1a5beef6
한민 <hanmin0786@gmail.com>
2023-05-08 18:22:20 +0900

    load data 함수 조금 짰음

--- header.h
+++ header.h
@@ -5,6 +5,7 @@
 typedef struct{
     int number; //일련번호
     int index; //삭제용 인덱스,  그냥 어레이 번호랑 같음
+    char callnum[20]; //전화번호
     char name[300]; //도서관 이름
     int localnum; //구 코드
     char localName[20]; //구 이름
@@ -19,6 +20,6 @@ void Modify(library *list[], int count);
 void Delete(library *list[], int count);
 void Recover(library *list[], int count);
 void Search(library *list[], int count);
-void LoadData();
+int LoadData(library *list[]);
 void SaveData(library *list[], int count);
 void Recommendation(library *list[], int count);
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "header.h"
 
 int main(){
-    
+    library *lib[2000];
+    int count = LoadData(lib);
 }
