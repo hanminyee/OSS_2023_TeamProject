1cd5db90cd37c11250c49179f1d665a37d8a724a
한민 <hanmin0786@gmail.com>
2023-05-14 12:51:38 +0900

    SelectMenu함수 만듦

--- main.c
+++ main.c
@@ -5,6 +5,43 @@ int main(){
     for(int i = 0; i<2000; i++){
         lib[i] = (library*)malloc(sizeof(library));
     }
+    printf("<<서울시 도서관 찾기 프로그램 L-Finder>>\n시작하시려면 아무 키나 눌러주세요!\n");
+    char dummy;
+    scanf("%c", &dummy);
+    getchar();
     int count = LoadData(lib);
-    Print(lib,count);
+    while(1){
+        SelectMenu();
+        int option;
+        scanf("%d", &option);
+        if(option==1){
+            Print(lib, count);
+        }
+        else if(option==2){
+            Create(lib, count);
+        }
+        else if(option==3){
+            Delete(lib, count);
+        }
+        else if(option==4){
+            Recover(lib, count);
+        }
+        else if(option==5){
+            Modify(lib, count);
+        }
+        else if(option==6){
+            Recommendation(lib, count);
+        }
+        else if(option==7){
+            SaveData(lib, count);
+        }
+        else if(option==8){
+            printf("프로그램을 종료합니다!");
+            exit(0);
+        }
+        else{
+            printf("잘못된 명령어입니다! 다시한번 확인해주세요!\n");
+            continue;
+        }
+    }
 }
